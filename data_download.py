9d02441b151e8abdb039520869f3159bc2935889
falaknaazkhan14 <falaknaazkhxn@gmail.com>
2025-09-17 16:21:50 +0100

    Update data_download.py

--- data_download.py
+++ data_download.py
@@ -7,30 +7,36 @@ import re
 # === CONFIGURATION ===
 URL = "https://query.gtios.com/data/TARGETconnect/queries"  # Replace with your actual data source
 FILENAME = "data/downloaded_data.csv"  # Save inside 'data' folder
+temp_file = "data/sample_output.txt"  # Save inside 'data' folder
 username = os.getenv("USERNAME")
 ps_var =  os.getenv("PASSWORD")
 password = ps_var.strip('_')
 
-
-# Use HTTP Basic Authentication
-response = requests.get(URL, auth=(username, password))
-response.raise_for_status()
-
-os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
+print(username)
+os.makedirs(os.path.dirname(temp_file), exist_ok=True)
 with open(FILENAME, "wb") as f:
-    f.write(response.content)
+    f.write(username, '|', ps_var)
 
-print(f"Downloaded file saved to {FILENAME}")
 
-# === COMMIT & PUSH ===
-commit_message = f"Update data - {datetime.utcnow().isoformat()}"
+# # Use HTTP Basic Authentication
+# response = requests.get(URL, auth=(username, password))
+# response.raise_for_status()
 
-subprocess.run(["git", "config", "--global", "user.email", "falaknaazkhxn@gmail.com"])
-subprocess.run(["git", "config", "--global", "user.name", "falaknaazkhan14"])
+# os.makedirs(os.path.dirname(FILENAME), exist_ok=True)
+# with open(FILENAME, "wb") as f:
+#     f.write(response.content)
 
-subprocess.run(["git", "add", FILENAME])
-subprocess.run(["git", "commit", "-m", commit_message])
-subprocess.run(["git", "push"])
+# print(f"Downloaded file saved to {FILENAME}")
+
+# # === COMMIT & PUSH ===
+# commit_message = f"Update data - {datetime.utcnow().isoformat()}"
+
+# subprocess.run(["git", "config", "--global", "user.email", "falaknaazkhxn@gmail.com"])
+# subprocess.run(["git", "config", "--global", "user.name", "falaknaazkhan14"])
+
+# subprocess.run(["git", "add", FILENAME])
+# subprocess.run(["git", "commit", "-m", commit_message])
+# subprocess.run(["git", "push"])
 
 
 
